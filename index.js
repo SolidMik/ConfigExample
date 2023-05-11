0969d9e1662a754908c880c1ea8708d5967a964f
SolidMik <86971618+SolidMik@users.noreply.github.com>
2023-05-11 17:11:44 +0100

    First upload
    Working, bulky code. Will need to trim it down due to a lot of repetitions. 
    Additionally will start expanding it to reflect agreed upon content.

---
+++ index.js
@@ -0,0 +1,197 @@
+//Remove first set of buttons - show student use cases
+function showStuWl() {
+    document.getElementById("stage1").classList.add("hide");
+    document.getElementById("stuWl").classList.remove("hide");
+} 
+
+//Remove first set of buttons - show hobbyist use cases
+function showHobWl() {
+    document.getElementById("stage1").classList.add("hide");
+    document.getElementById("hobWl").classList.remove("hide");
+}
+
+//Remove first set of buttons - show professional use cases
+function showProWl() {
+    document.getElementById("stage1").classList.add("hide");
+    document.getElementById("proWl").classList.remove("hide");
+}
+
+//Remove use case buttons - show gaming budget
+function showGamBd() {
+    document.getElementById("stuWl").classList.add("hide");
+    document.getElementById("stuGam").classList.remove("hide");
+}
+
+//Remove use case buttons - show render budget
+function showRenBd() {
+    document.getElementById("stuWl").classList.add("hide");
+    document.getElementById("stuRen").classList.remove("hide");
+}
+
+//Remove use case buttons - show hosting budget
+function showHosBd() {
+    document.getElementById("stuWl").classList.add("hide");
+    document.getElementById("stuHos").classList.remove("hide");
+}
+
+//Remove use case buttons - show overclocking budget
+function showOveBd() {
+    document.getElementById("hobWl").classList.add("hide");
+    document.getElementById("hobOve").classList.remove("hide");
+}
+
+//Remove use case buttons - show video budget
+function showVidBd() {
+    document.getElementById("hobWl").classList.add("hide");
+    document.getElementById("hobVid").classList.remove("hide");
+}
+
+//Remove use case buttons - show low lvl AI budget
+function showLlaBd() {
+    document.getElementById("hobWl").classList.add("hide");
+    document.getElementById("hobAi").classList.remove("hide");
+}
+
+//Remove use case buttons - show REASEARCH budget
+function showReBd() {
+    document.getElementById("proWl").classList.add("hide");
+    document.getElementById("proRe").classList.remove("hide");
+}
+
+//Remove use case buttons - show AI budget
+function showAiBd() {
+    document.getElementById("proWl").classList.add("hide");
+    document.getElementById("proAI").classList.remove("hide");
+}
+
+//Remove use case buttons - show Cloud budget
+function showClBd() {
+    document.getElementById("proWl").classList.add("hide");
+    document.getElementById("proCL").classList.remove("hide");
+}
+
+//Display gaming products
+function displayLg() {
+    document.getElementById("stuGam").classList.add("hide");
+    document.getElementById("Lg").classList.remove("hide");
+} 
+function displayMg() {
+    document.getElementById("stuGam").classList.add("hide");
+    document.getElementById("Mg").classList.remove("hide");
+} 
+function displayIg() {
+    document.getElementById("stuGam").classList.add("hide");
+    document.getElementById("Ig").classList.remove("hide");
+} 
+
+//Display rendering products
+function displayLr() {
+    document.getElementById("stuRen").classList.add("hide");
+    document.getElementById("Mg").classList.remove("hide");
+} 
+function displayMr() {
+    document.getElementById("stuRen").classList.add("hide");
+    document.getElementById("Mr").classList.remove("hide");
+} 
+function displayIr() {
+    document.getElementById("stuRen").classList.add("hide");
+    document.getElementById("Ig").classList.remove("hide");
+} 
+
+//Display hosting products
+function displayLh() {
+    document.getElementById("stuHos").classList.add("hide");
+    document.getElementById("Lh").classList.remove("hide");
+} 
+function displayMh() {
+    document.getElementById("stuHos").classList.add("hide");
+    document.getElementById("Mh").classList.remove("hide");
+} 
+function displayIh() {
+    document.getElementById("stuHos").classList.add("hide");
+    document.getElementById("Ih").classList.remove("hide");
+} 
+
+//Display Overclocking products
+function displayLo() {
+    document.getElementById("hobOve").classList.add("hide");
+    document.getElementById("Mh").classList.remove("hide");
+} 
+function displayMo() {
+    document.getElementById("hobOve").classList.add("hide");
+    document.getElementById("Ih").classList.remove("hide");
+} 
+function displayIo() {
+    document.getElementById("hobOve").classList.add("hide");
+    document.getElementById("Io").classList.remove("hide");
+} 
+
+//Display Video editing products
+function displayLv() {
+    document.getElementById("hobVid").classList.add("hide");
+    document.getElementById("Mr").classList.remove("hide");
+} 
+function displayMv() {
+    document.getElementById("hobVid").classList.add("hide");
+    document.getElementById("Ig").classList.remove("hide");
+} 
+function displayIv() {
+    document.getElementById("hobVid").classList.add("hide");
+    document.getElementById("Iv").classList.remove("hide");
+} 
+
+//Display low LVL AI products
+function displayLa() {
+    document.getElementById("hobAi").classList.add("hide");
+    document.getElementById("Mr").classList.remove("hide");
+} 
+function displayMa() {
+    document.getElementById("hobAi").classList.add("hide");
+    document.getElementById("Ig").classList.remove("hide");
+} 
+function displayIa() {
+    document.getElementById("hobAi").classList.add("hide");
+    document.getElementById("Iv").classList.remove("hide");
+} 
+
+//Display PRO RES products
+function displayLre() {
+    document.getElementById("proRe").classList.add("hide");
+    document.getElementById("Ih").classList.remove("hide");
+} 
+function displayMre() {
+    document.getElementById("proRe").classList.add("hide");
+    document.getElementById("Io").classList.remove("hide");
+} 
+function displayIre() {
+    document.getElementById("proRe").classList.add("hide");
+    document.getElementById("Ire").classList.remove("hide");
+} 
+
+//Display PRO AI products
+function displayLai() {
+    document.getElementById("proAI").classList.add("hide");
+    document.getElementById("Ig").classList.remove("hide");
+} 
+function displayMai() {
+    document.getElementById("proAI").classList.add("hide");
+    document.getElementById("Iv").classList.remove("hide");
+} 
+function displayIai() {
+    document.getElementById("proAI").classList.add("hide");
+    document.getElementById("Iai").classList.remove("hide");
+} 
+
+//Display PRO CLOUD products
+function displayLc() {
+    document.getElementById("proCL").classList.add("hide");
+    document.getElementById("Io").classList.remove("hide");
+} 
+function displayMc() {
+    document.getElementById("proCL").classList.add("hide");
+    document.getElementById("Mcl").classList.remove("hide");
+} 
+function displayIc() {
+    document.getElementById("proCL").classList.add("hide");
+    document.getElementById("Ire").classList.remove("hide");
+} 
